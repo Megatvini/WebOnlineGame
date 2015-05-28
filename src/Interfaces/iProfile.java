f0af333bcd29c3840800fb99c80b684596016015
გუკა <guka.mazanashvili@live.com>
2015-05-28 04:13:01 +0400

    პროფილის ინტერფეისი

---
+++ src/Interfaces/iProfile.java
@@ -0,0 +1,18 @@
+package Interfaces;
+
+/**
+ * Created by gukam on 5/28/2015.
+ */
+public interface iProfile {
+    public String getNickname();
+
+    public String getPicturePath();
+
+    public String getFirstname();
+
+    public String getLastname();
+
+    public String getMail();
+
+    public String getSex();
+}
