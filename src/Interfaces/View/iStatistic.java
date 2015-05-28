9b38735e2401dfb40b474ed33c978e2a8acd785d
გუკა <guka.mazanashvili@live.com>
2015-05-28 13:12:06 +0400

    დავამატე statistic-ის ინტერფეისი view-ში

---
+++ src/Interfaces/View/iStatistic.java
@@ -0,0 +1,12 @@
+package Interfaces.View;
+
+/**
+ * Created by gukam on 5/28/2015.
+ */
+public interface iStatistic {
+    public int online();
+
+    public int newAccountsNum();
+
+    public int matchCount();
+}
