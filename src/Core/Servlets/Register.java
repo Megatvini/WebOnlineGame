17f1b7ffa125f76391a72e6295f7845b1af47bd4
გუკა <guka.mazanashvili@live.com>
2015-05-29 20:35:36 +0400

    შევქმენი Registration სერვლეტი

---
+++ src/Core/Servlets/Register.java
@@ -0,0 +1,22 @@
+package Core.Servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by gukam on 5/29/2015.
+ */
+@WebServlet("/Registration")
+public class Register extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
