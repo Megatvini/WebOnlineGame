16476669ed38d791826880c52035a1d80b62b465
Rshalika <rshal13@freeuni.edu.ge>
2015-05-29 19:32:58 +0400

    daemata cookies amogeba  momxmareblidan

--- web/client.js
+++ web/client.js
@@ -85,6 +85,26 @@ var Client = IgeClass.extend({
 
 				if (success) {
 
+
+
+					var str = getCookie("playerID");
+					function getCookie(cname) {
+						var name = cname + "=";
+						var ca = document.cookie.split(';');
+						for(var i=0; i<ca.length; i++) {
+							var c = ca[i];
+							while (c.charAt(0)==' ') c = c.substring(1);
+							if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+						}
+						return "";
+					}
+
+
+
+
+
+
+
 					initial();
 
 					var connection = initSocket();
