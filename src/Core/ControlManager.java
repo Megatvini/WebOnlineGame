e78167de1daa0188780600070269ef0d17e92b81
გუკა <guka.mazanashvili@live.com>
2015-05-31 03:08:39 +0400

    logout-იც მუშაობს

--- src/Core/ControlManager.java
+++ src/Core/ControlManager.java
@@ -7,7 +7,7 @@ import Interfaces.Controller.iAccount;
 /**
  * Created by gukam on 5/29/2015.
  */
-public class ControlManager {
+public class ControlManager { //user unda erqvas
     public static iAccount getAccount(String nickname){
         return (new Account(nickname));
     }
