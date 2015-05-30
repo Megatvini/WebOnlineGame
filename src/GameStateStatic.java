c82b61b1381cd08f62c91b18f33f8048ae81fae5
martlaboloa <saros13@freeuni.edu.ge>
2015-05-30 15:52:24 +0400

    Model: world, araa damtavrebuli, araa testebi, aris zedmeti failebi.

--- src/GameStateStatic.java
+++ src/GameStateStatic.java
@@ -1,19 +1,19 @@
-//import java.util.ArrayList;
-//
-///**
-// *
-// */
-//public class GameStateStatic {
-//
-//    private static  GameState gameState  = null;
-//    private GameStateStatic(){
-//       gameState = new GameState();
-//    }
-//    public static GameState getState(){
-//        if (gameState==null) {
-//            new GameStateStatic();
-//        }
-//        return gameState;
-//
-//    }
-//}
+import java.util.ArrayList;
+
+/**
+ *
+ */
+public class GameStateStatic {
+
+    private static  GameState gameState  = null;
+    private GameStateStatic(){
+       gameState = new GameState();
+    }
+    public static GameState getState(){
+        if (gameState==null) {
+            new GameStateStatic();
+        }
+        return gameState;
+
+    }
+}
--- src/Player.java
+++ src/Player.java
@@ -1,64 +1,64 @@
-//import com.google.gson.Gson;
-//import com.google.gson.annotations.Expose;
-//
-//
-//public class Player {
-//    @Expose
-//    private double x;
-//    @Expose
-//    private double y;
-//    @Expose
-//    private int id ;
-//
-//
-//
-//
-//
-//    public Player(int id, double x, double y) {
-//        this.id = id;
-//        this.y = y;
-//        this.x = x;
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public double getX() {
-//        return x;
-//    }
-//
-//    public void setX(double x) {
-//        this.x = x;
-//    }
-//
-//    public double getY() {
-//        return y;
-//    }
-//
-//    public void setY(double y) {
-//        this.y = y;
-//    }
-//
-//
-//    @Override
-//    public String toString() {
-//        return new Gson().toJson(this);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        int result;
-//        long temp;
-//        temp = Double.doubleToLongBits(x);
-//        result = (int) (temp ^ (temp >>> 32));
-//        temp = Double.doubleToLongBits(y);
-//        result = 31 * result + (int) (temp ^ (temp >>> 32));
-//        result = 31 * result + id;
-//        return result;
-//    }
-//}
+import com.google.gson.Gson;
+import com.google.gson.annotations.Expose;
+
+
+public class Player {
+    @Expose
+    private double x;
+    @Expose
+    private double y;
+    @Expose
+    private int id ;
+
+
+
+
+
+    public Player(int id, double x, double y) {
+        this.id = id;
+        this.y = y;
+        this.x = x;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(x);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(y);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + id;
+        return result;
+    }
+}
