a9cecc24d3d3f517f35aca3879b130785071afad
Nika Doghonadze <ndogh13@freeuni.edu.ge>
2015-06-29 08:11:03 +0400

    დაემატა ტესტები

--- Test/Core/Controller/GameManagerTest.java
+++ Test/Core/Controller/GameManagerTest.java
@@ -1,8 +1,16 @@
 package Core.Controller;
 
+import Core.Bean.Account;
+import Core.Dao.AccountDao;
+import Core.Dao.GameDao;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
 
 /**
  * Created by Nika on 00:55, 6/29/2015.
@@ -10,6 +18,23 @@ import static org.junit.Assert.*;
 public class GameManagerTest {
     @Test
     public void testAddNewGameResults() throws Exception {
+        AccountDao accountDaoMock = mock(AccountDao.class);
+        GameDao gameDaoMock = mock(GameDao.class);
+
+        GameManager gameManager = new GameManager(accountDaoMock, gameDaoMock);
+        List<String> gameResult = new ArrayList<>();
+        gameResult.add("nika");
+        gameResult.add("rezo");
+        gameResult.add("shako");
+        gameResult.add("guka");
+        when(accountDaoMock.getUser(any())).thenReturn(new Account());
+        gameManager.addNewGameResults(gameResult);
+        for (String s : gameResult) {
+            verify(accountDaoMock).getUser(s);
+        }
+        verify(accountDaoMock, times(4)).changeUser(any());
+        verify(gameDaoMock).addNewGame(any());
+
 
     }
 }
